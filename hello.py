21de838b9829f6a02bd6dbed8e56003feda70a15
Anik Banerjee <anik.banerjee1@ibm.com>
2026-06-22 22:39:13 +0530

    Add hello.py

---
+++ hello.py
@@ -0,0 +1,12 @@
+print ("Hello, World!")
+name = input("What is your name? ")
+print(f"Hello, {name}!")
+
+age = input("What is your age? ")
+age = int(age)
+years_remaining = 100 - age
+print(f"You have {years_remaining} years until 100!")
+
+age = int(input("What is your age? "))
+years_remaining = 100 - age
+print(f"You have {years_remaining} years until 100!")
